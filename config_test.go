9fb7bb842c4a9b829c38334d07ee5ca383b48aea
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-06-05 20:23:52 +0900

    refactor test for Config

--- config_test.go
+++ config_test.go
@@ -7,32 +7,59 @@ import (
 )
 
 func TestNewConfig(t *testing.T) {
-	var newConfigTestCases = []struct {
+	type input struct {
 		hasher   hash.Hash
 		depth    int
 		hashSize int
-		config   *Config
-		err      error
+	}
+	type output struct {
+		config *Config
+		err    error
+	}
+	var newConfigTestCases = []struct {
+		in  input
+		out output
 	}{
-		{sha256.New(), 1, 1, &Config{allLeavesNum: 2, allNodesNum: 3}, nil},
-		{sha256.New(), 16, 64, &Config{allLeavesNum: 65536, allNodesNum: 131071}, nil},
-		{sha256.New(), 0, 32, nil, ErrTooSmallDepth},
-		{sha256.New(), 17, 32, nil, ErrTooLargeDepth},
-		{sha256.New(), 8, 0, nil, ErrTooSmallHashSize},
-		{sha256.New(), 8, 65, nil, ErrTooLargeHashSize},
+		{
+			input{sha256.New(), 1, 1},
+			output{&Config{allLeavesNum: 2, allNodesNum: 3}, nil},
+		},
+		{
+			input{sha256.New(), 16, 64},
+			output{&Config{allLeavesNum: 65536, allNodesNum: 131071}, nil},
+		},
+		{
+			input{sha256.New(), 0, 32},
+			output{nil, ErrTooSmallDepth},
+		},
+		{
+			input{sha256.New(), 17, 32},
+			output{nil, ErrTooLargeDepth},
+		},
+		{
+			input{sha256.New(), 8, 0},
+			output{nil, ErrTooSmallHashSize},
+		},
+		{
+			input{sha256.New(), 8, 65},
+			output{nil, ErrTooLargeHashSize},
+		},
 	}
 
 	for _, tc := range newConfigTestCases {
-		conf, err := NewConfig(tc.hasher, tc.depth, tc.hashSize)
-		if err != tc.err {
-			t.Errorf("expected: %v, actual: %v", tc.err, err)
+		in, out := tc.in, tc.out
+
+		conf, err := NewConfig(in.hasher, in.depth, in.hashSize)
+		if err != out.err {
+			t.Errorf("expected: %v, actual: %v", out.err, err)
 		}
+
 		if conf != nil {
-			if conf.allLeavesNum != tc.config.allLeavesNum {
-				t.Errorf("expected: %d, actual: %d", tc.config.allLeavesNum, conf.allLeavesNum)
+			if conf.allLeavesNum != out.config.allLeavesNum {
+				t.Errorf("expected: %d, actual: %d", out.config.allLeavesNum, conf.allLeavesNum)
 			}
-			if conf.allNodesNum != tc.config.allNodesNum {
-				t.Errorf("expected: %d, actual: %d", tc.config.allNodesNum, conf.allNodesNum)
+			if conf.allNodesNum != out.config.allNodesNum {
+				t.Errorf("expected: %d, actual: %d", out.config.allNodesNum, conf.allNodesNum)
 			}
 		}
 	}
