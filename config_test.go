06e9cc9ea1d06911080cf864dc192b7357d86f3f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-06-05 21:08:37 +0900

    output test name

--- config_test.go
+++ config_test.go
@@ -17,36 +17,44 @@ func TestNewConfig(t *testing.T) {
 		err    error
 	}
 	var newConfigTestCases = []struct {
-		in  input
-		out output
+		name string
+		in   input
+		out  output
 	}{
 		{
+			"success: min size",
 			input{sha256.New(), 1, 1},
 			output{&Config{allLeavesNum: 2, allNodesNum: 3}, nil},
 		},
 		{
+			"success: max size",
 			input{sha256.New(), 16, 64},
 			output{&Config{allLeavesNum: 65536, allNodesNum: 131071}, nil},
 		},
 		{
+			"failure: too small depth",
 			input{sha256.New(), 0, 32},
 			output{nil, ErrTooSmallDepth},
 		},
 		{
+			"failure: too large depth",
 			input{sha256.New(), 17, 32},
 			output{nil, ErrTooLargeDepth},
 		},
 		{
+			"failure: too small hash size",
 			input{sha256.New(), 8, 0},
 			output{nil, ErrTooSmallHashSize},
 		},
 		{
+			"failure: too large hash size",
 			input{sha256.New(), 8, 65},
 			output{nil, ErrTooLargeHashSize},
 		},
 	}
 
-	for _, tc := range newConfigTestCases {
+	for i, tc := range newConfigTestCases {
+		t.Logf("[%d] %s", i, tc.name)
 		in, out := tc.in, tc.out
 
 		conf, err := NewConfig(in.hasher, in.depth, in.hashSize)
