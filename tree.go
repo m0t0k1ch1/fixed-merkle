480dae8acc5e37a1988e8791f463cc8d0e5b97cd
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-21 19:22:42 +0900

    update

--- tree.go
+++ tree.go
@@ -3,27 +3,56 @@ package merkle
 import (
 	"bytes"
 	"errors"
+	"hash"
+	"math/big"
+)
+
+const (
+	DepthMax = 32
 )
 
 var (
-	ErrTooManyLeaves       = errors.New("number of leaves exceeds upper limit")
-	ErrLeafIndexOutOfRange = errors.New("leaf index is out of range")
+	ErrTooLargeDepth     = errors.New("too large tree depth")
+	ErrTooManyLeaves     = errors.New("too many leaves")
+	ErrTooLargeLeafIndex = errors.New("too large leaf index")
+	ErrInvalidProofSize  = errors.New("invalid proof size")
 )
 
 type Tree struct {
-	config *Config
-	nodes  []*Node
-	levels [][]*Node
+	hasher       hash.Hash
+	hashSize     uint64
+	depth        uint64
+	allLeavesNum uint64
+	allNodesNum  uint64
+	nodes        []*Node
+	levels       [][]*Node
 }
 
-func NewTree(conf *Config, leaves [][]byte) (*Tree, error) {
-	if uint64(len(leaves)) > conf.allLeavesNum {
+func NewTree(hasher hash.Hash, depth uint64, leaves [][]byte) (*Tree, error) {
+	if depth > DepthMax {
+		return nil, ErrTooLargeDepth
+	}
+
+	allLeavesNum := new(big.Int).Lsh(big.NewInt(2), uint(depth-1)).Uint64()
+	if uint64(len(leaves)) > allLeavesNum {
 		return nil, ErrTooManyLeaves
 	}
 
-	tree := &Tree{
-		config: conf,
+	allNodesNum := uint64(0)
+	for i := allLeavesNum; i >= 1; i /= 2 {
+		allNodesNum += i
 	}
+
+	tree := &Tree{
+		hasher:       hasher,
+		hashSize:     uint64(hasher.Size()),
+		depth:        depth,
+		allLeavesNum: allLeavesNum,
+		allNodesNum:  allNodesNum,
+		nodes:        make([]*Node, allNodesNum),
+		levels:       make([][]*Node, depth+1),
+	}
+
 	if err := tree.buildBase(leaves); err != nil {
 		return nil, err
 	}
@@ -34,47 +63,57 @@ func NewTree(conf *Config, leaves [][]byte) (*Tree, error) {
 	return tree, nil
 }
 
+func (tree *Tree) hash(b []byte) ([]byte, error) {
+	tree.hasher.Reset()
+	if _, err := tree.hasher.Write(b); err != nil {
+		return nil, err
+	}
+	return tree.hasher.Sum(nil), nil
+}
+
+func (tree *Tree) pairHash(b1, b2 []byte) ([]byte, error) {
+	tree.hasher.Reset()
+	if _, err := tree.hasher.Write(b1); err != nil {
+		return nil, err
+	}
+	if _, err := tree.hasher.Write(b2); err != nil {
+		return nil, err
+	}
+	return tree.hasher.Sum(nil), nil
+}
+
 func (tree *Tree) buildBase(leaves [][]byte) error {
-	conf := tree.config
-	leavesNum := len(leaves)
+	leavesNum := uint64(len(leaves))
 
-	tree.nodes = make([]*Node, conf.allNodesNum)
-	tree.levels = make([][]*Node, conf.depth+1)
-	tree.levels[conf.depth] = make([]*Node, conf.allLeavesNum)
+	tree.levels[tree.depth] = make([]*Node, tree.allLeavesNum)
 
-	for i := 0; i < leavesNum; i++ {
-		conf.hasher.Reset()
-		if _, err := conf.hasher.Write(leaves[i]); err != nil {
+	for i := uint64(0); i < leavesNum; i++ {
+		leafHash, err := tree.hash(leaves[i])
+		if err != nil {
 			return err
 		}
-		leafHash := conf.hasher.Sum(nil)
 
 		node := newNode(leafHash, nil, nil)
 		tree.nodes[i] = node
-		tree.levels[conf.depth][i] = node
+		tree.levels[tree.depth][i] = node
 	}
 
-	emptyLeaf := make([]byte, conf.hashSize, conf.hashSize)
-
-	conf.hasher.Reset()
-	if _, err := conf.hasher.Write(emptyLeaf); err != nil {
+	emptyLeafHash, err := tree.hash(make([]byte, tree.hashSize, tree.hashSize))
+	if err != nil {
 		return err
 	}
-	emptyLeafHash := conf.hasher.Sum(nil)
 
-	for i := uint64(leavesNum); i < conf.allLeavesNum; i++ {
+	for i := leavesNum; i < tree.allLeavesNum; i++ {
 		node := newNode(emptyLeafHash, nil, nil)
 		tree.nodes[i] = node
-		tree.levels[conf.depth][i] = node
+		tree.levels[tree.depth][i] = node
 	}
 
 	return nil
 }
 
 func (tree *Tree) build() error {
-	conf := tree.config
-
-	for d := conf.depth; d > 0; d-- {
+	for d := tree.depth; d > 0; d-- {
 		level := tree.levels[d]
 
 		nextDepth := d - 1
@@ -84,14 +123,12 @@ func (tree *Tree) build() error {
 			left := level[i]
 			right := level[i+1]
 
-			tree.config.hasher.Reset()
-			b := concat(left.Bytes(), right.Bytes())
-			if _, err := conf.hasher.Write(b); err != nil {
+			parentHash, err := tree.pairHash(left.Bytes(), right.Bytes())
+			if err != nil {
 				return err
 			}
-			b = conf.hasher.Sum(nil)
 
-			tree.levels[nextDepth][i/2] = newNode(b, left, right)
+			tree.levels[nextDepth][i/2] = newNode(parentHash, left, right)
 		}
 
 		tree.nodes = append(tree.nodes, tree.levels[nextDepth]...)
@@ -101,27 +138,17 @@ func (tree *Tree) build() error {
 }
 
 func (tree *Tree) Root() *Node {
-	return tree.Level(0)[0]
-}
-
-func (tree *Tree) Level(depth uint64) []*Node {
-	if tree.config.depth < depth {
-		return nil
-	}
-
-	return tree.levels[depth]
+	return tree.levels[0][0]
 }
 
 func (tree *Tree) CreateMembershipProof(index uint64) ([]byte, error) {
-	conf := tree.config
-
-	if conf.allLeavesNum <= index {
-		return nil, ErrLeafIndexOutOfRange
+	if index >= tree.allLeavesNum {
+		return nil, ErrTooLargeLeafIndex
 	}
 
 	buf := bytes.NewBuffer(nil)
 
-	for d := conf.depth; d > 0; d-- {
+	for d := tree.depth; d > 0; d-- {
 		var siblingIndex uint64
 		if index%2 == 0 {
 			siblingIndex = index + 1
@@ -141,31 +168,31 @@ func (tree *Tree) CreateMembershipProof(index uint64) ([]byte, error) {
 }
 
 func (tree *Tree) VerifyMembershipProof(index uint64, proof []byte) (bool, error) {
-	conf := tree.config
-
-	if conf.allLeavesNum <= index {
-		return false, ErrLeafIndexOutOfRange
+	if index >= tree.allLeavesNum {
+		return false, ErrTooLargeLeafIndex
+	}
+	if uint64(len(proof)) != tree.depth*tree.hashSize {
+		return false, ErrInvalidProofSize
 	}
 
-	if uint64(len(proof)) != conf.depth*conf.hashSize {
-		return false, nil
-	}
+	proofIndex := uint64(0)
 
-	b := tree.levels[conf.depth][index].Bytes()
-	for i := uint64(0); i < conf.depth; i++ {
-		sibling := proof[i*conf.hashSize : i*conf.hashSize+conf.hashSize]
+	b := tree.levels[tree.depth][index].Bytes()
 
-		conf.hasher.Reset()
+	for d := tree.depth; d > 0; d-- {
+		sibling := proof[proofIndex : proofIndex+tree.hashSize]
+
+		var err error
 		if index%2 == 0 {
-			b = concat(b, sibling)
+			b, err = tree.pairHash(b, sibling)
 		} else {
-			b = concat(sibling, b)
+			b, err = tree.pairHash(sibling, b)
 		}
-		if _, err := conf.hasher.Write(b); err != nil {
+		if err != nil {
 			return false, err
 		}
-		b = conf.hasher.Sum(nil)
 
+		proofIndex += tree.hashSize
 		index /= 2
 	}
 
